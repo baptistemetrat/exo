6041485ed872be63ad029d8f209aa3015ea951c5
Baptiste Metrat <baptiste.metrat@gmail.com>
2017-03-07 16:52:56 +0100

    eslint correction

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	var express = require('express'); // charge ExpressJS
-	var serveIndex = require('serve-index');
+    var express = require('express'); // charge ExpressJS
+    var serveIndex = require('serve-index');
 
-	var app = express();
-	
-	app.use(express.static('.'));
-	app.use(serveIndex('.', {icons: true}));
-	
-	app.use('/app/', function(req, res, next){
-		console.log('req.url', req.url);
-		res.sendFile('./app/exo3/index.html', {root: __dirname});
-	});
+    var app = express();
 
-	app.use(function(req, res, next) {
-		console.log('404: Page not Found', req.url);
-		next();
-	});
+    app.use(express.static('.'));
+    app.use(serveIndex('.', {icons: true}));
 
-	app.listen(8000, function() {
-		console.log('server started on port 8000');
-	});
+    app.use('/app/', function(req, res, next) {
+        console.log('req.url', req.url);
+        res.sendFile('./app/exo3/index.html', {root: __dirname});
+    });
+
+    app.use(function(req, res, next) {
+        console.log('404: Page not Found', req.url);
+        next();
+    });
+
+    app.listen(8000, function() {
+        console.log('server started on port 8000');
+    });
 })();
